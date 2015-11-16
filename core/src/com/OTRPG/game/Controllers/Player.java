ee8dc4b7d3d42f2616d8231fb86371ad5383b2c6
cypher31 <kelbystyler@gmail.com>
2015-11-16 12:40:28 -0800

    11/16/15
    Added folder for various controllers - starting on player controller
    first

---
+++ core/src/com/OTRPG/game/Controllers/Player.java
@@ -0,0 +1,5 @@
+package com.OTRPG.game.Controllers;
+
+public class Player {
+
+}
--- core/src/com/OTRPG/game/MyOneTapGame.java
+++ core/src/com/OTRPG/game/MyOneTapGame.java
@@ -1,13 +1,9 @@
 package com.OTRPG.game;
 
-import javafx.scene.transform.Scale;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
